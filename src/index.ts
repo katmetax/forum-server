b23715d8977e140783e158448297b71134ca59f2
Katerina Metaxopoulou <kat@katmetax.com>
2022-04-24 17:40:34 +0100

    Add post resolver

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import { buildSchema } from 'type-graphql';
 import mikroConfig from './mikro-orm.config';
+import { PostResolver } from './resolvers/post';
 
 const port = 4000;
 
@@ -9,6 +12,18 @@ export const Main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
   app.listen(port, () => {
     console.log('server started at localhost:', port);
   });
